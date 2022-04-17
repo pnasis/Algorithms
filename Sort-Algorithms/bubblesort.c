226d4b450c588c072b266f4f5d45610a876cc965
Prodromos Nasis <79412734+pnasis@users.noreply.github.com>
2022-04-17 13:12:04 +0300

    Update bubblesort.c

--- Sort-Algorithms/bubblesort.c
+++ Sort-Algorithms/bubblesort.c
@@ -1,47 +1,41 @@
 /*
-Όνομα: selectsort
-Είσοδος: Ο πίνακας Τ με τα στοιχεία προς ταξινόμηση και η διάσταση n του Τ
-Έξοδος: Ο πίνακας Τ ταξινομημένος
-Perigrafi: Ο αλγόριθμος ταξινομεί τα στοιχεία ενός πίνακα Τ διάστασης n. 
-   Ο αλγόριθμος σε κάθε βήμα σαρώνει τον πίνακα Τ και εναλλάσει τα γειτονικά
-   στοιχεία που έχουν λάθος διάταξη.
+DESCRIPTION: The algorithm scans the array and swaps "neighbors" that are in the wrong possision.
 */
 
 #include <stdio.h>
-#define N 9 /* Διάσταση του πίνακα Τ */
+#define N 9
 
-/* Δήλωση συνάρτησης */
 void bubblesort(double T[], int n);
 
 main(){
     int i;
-    double T[]={10,1,15,8,9,6,3,1,7}; /* Τα στοιχεία του Τ */
+    double T[]={10,1,15,8,9,6,3,1,7};
 
-    /* Εκτύπωση αρχικού πίνακα */
-    printf("Arxikos pinakas:");
+    /*Printing the original array*/
+    printf("Original array:");
     for(i=0;i<N;i++)
         printf(" %f",T[i]);
-    /* Κλήση της bubblesort */
+    /*We call the bubblesort function*/
     bubblesort(T,N);
-    /* Εκτύπωση του ταξινομημένου πίνακα */
-    printf("\nTaxinomimenos pinakas:");
+    /*Printing the sorted array*/
+    printf("\nSorted array:");
     for(i=0;i<N;i++)
         printf(" %f",T[i]);
     printf("\n");
     system("pause");
 }
 
-/* Ο αλγόριθμος bubblesort */
+/*Bubblesort Function*/
 void bubblesort(double T[], int n){
     int i=n;
     int t=-1;
     int j;
-    double temp; /* Προσωρινή μεταβλητή */
+    double temp; /*Temporary variable*/
     while (i>=1 && t!=0){
         t=0;
         for (j=0;j<i-1;j++)
             if(T[j]>T[j+1]) {
-                temp=T[j];  /* Εναλλαγή των T[j], T[j+1]*/
+                temp=T[j];  /* We swap T[j] with T[j+1]*/
                 T[j]=T[j+1];
                 T[j+1]=temp;
                 t++;
